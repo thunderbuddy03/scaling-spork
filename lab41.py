714e2bbd1e371d63afaa3c17720da5f8a10f72f3
thunderbuddy03 <35676523+thunderbuddy03@users.noreply.github.com>
2018-01-31 10:32:55 +0530

    Add files via upload

---
+++ lab41.py
@@ -0,0 +1,17 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> (2+3)*4+8-6**2
+-8
+>>> n=7
+>>> 
+================= RESTART: /home/cs2017a203/pradeep/lab4.py =================
+3
+>>> 
+================= RESTART: /home/cs2017a203/pradeep/lab4.py =================
+3
+>>> 2 or 3
+2
+>>> 2 and 3
+3
+>>> 
