d7ccfdfccafdf0c690cacc66f6c9456ee4d1e8fb
thunderbuddy03 <35676523+thunderbuddy03@users.noreply.github.com>
2018-02-14 09:56:32 +0530

    Delete lab82.py

--- lab82.py
+++
@@ -1,9 +0,0 @@
-import smtplib
- 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login("pradeepselvaraj10t@gmail.com", "@selvaraj10t")
- 
-msg = "hey how are you"
-server.sendmail("pradeepselvaraj10t@gmail.com", "nithind7026@gmail.com", msg)
-server.quit()
